34ebc68fd8356e62c16b834c4eef721a22ff4369
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2022-05-14 00:52:23 -0300

    Listo para revision

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,57 @@
-import { Button, Form } from 'react-bootstrap'
 import './login.css'
 import imgSelect from '../img/descarga.png'
 const Login = () => {
   return (
     <div className='fondo'>
-      <navbar className='img-contenedor2'>
+      <header id='img-contenedor2'>
           <img src={imgSelect}/>
           <h6>Experiencia laboral</h6>
           <p>¿Tienes experiencia que añadir?</p>
-      </navbar>
-      <div className='form'>
-      <Form className='zindex-2'>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Prueba</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+      </header>
+      <main className='contenedor-form'>
+      <form className='form'>
+        <fieldset id='field-hijo-1'>
+        <h3>Datos personales</h3>
+        <label>Nombre</label>
+        <input type="text"/>
+        <label>Apellido</label>
+        <input type="text" />
+        <label>Email</label>
+        <input type="text" />
+        <label>Pais de residencia</label>
+        <input type="text" />
+        <label>DNI</label>
+        <input type="text" />
+        <label>Nacionalidad</label>
+        <input type="text" />
+        <label>Teléfono</label>
+        <input type='text'/>
+        <select>
+          <option>Celular/Móvil</option>
+          <option>Fijo</option>
+        </select>
+        <label>Fecha de nacimiento</label>
+        <input type="text" />
+        <label>Género</label>
+        <input type="text" />
+        </fieldset>
+        <fieldset id='field-hijo-2'>
+          <h3>Trabajo y educacion</h3>
+          <label>Adjuntar CV</label>
+          <input type='file'/>
+        </fieldset>
+        
+        <fieldset id='field-hijo-3'>
+          <h3>Expectativas personales</h3>
+          <label>Remuneracion pretendida:</label>
+          <input type='number'/>
+          <label>Mensaje adicional</label>
+          <textarea placeholder='Mensaje adicional para el reclutador'>
+          </textarea>
+        </fieldset>
+      <button>Enviar</button>
+      </form>
+      </main>
 
     </div>
   )
